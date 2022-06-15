d6cf3f017a655e8c6ba0e0c50fa8f455583685cc
Kaue Andrade Padilha <104653126+Kaue-Dev@users.noreply.github.com>
2022-06-15 11:50:52 -0300

    Add files via upload

---
+++ consultorio/consultorio.js
@@ -0,0 +1,30 @@
+//Criar referencia aos elementos do HTML
+const frm = document.querySelector("form")
+const respNome = document.querySelector("span")
+const respLista = document.querySelector("pre")
+
+const pacientes = [] //Declara o vetor global
+
+frm.addEventListener("submit", (e) => {
+    e.preventDefault() //Formulario não enviar dados
+    const nome = frm.inPaciente.value //Obtem o nome do paciente
+    pacientes.push(nome) //Adiciona elemento no final do vetor
+    console.log(pacientes)
+    let lista = "" //String para concaternar pacientes
+    for (let i = 0; i < pacientes.length; i++) {
+        lista += `${i + 1}. ${pacientes[i]}\n`
+    }
+    respLista.innerText = lista //Exibe a lista de pacientes
+    frm.inPaciente.value =  "" //Limpa o conteúdo do campo
+    frm.inPaciente.focus() //Posiciona o cursor no campo
+})
+
+frm.btnUrgencia.addEventListener("click", () => {
+    const nome = frm.inPaciente.value //Obtem o nome do paciente
+    pacientes.unshift(nome) //Adiciona o paciente no inicio do vetor
+    let lista = "" //String para concatenar
+    pacientes.forEach((paciente, i) => (lista += `${i + 1}. ${pacientes[i]}\n`))
+    respLista.innerText = lista //Exibe a lista de pacientes
+    frm.inPaciente.value =  "" //Limpa o conteúdo do campo
+    frm.inPaciente.focus() //Posiciona o cursor no campo
+})
